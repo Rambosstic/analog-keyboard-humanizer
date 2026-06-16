463881366c83866a4f8ab0f92e2cb257410432b4
Moofda <mikejshawn@protonmail.com>
2026-06-15 21:06:08 -0700

    Update humanizer.h

--- src/humanizer.h
+++ src/humanizer.h
@@ -1,39 +1,24 @@
-#ifndef _HUMANIZER_H_
-#define _HUMANIZER_H_
+#ifndef HUMANIZER_H
+#define HUMANIZER_H
 
 #include <stdint.h>
-#include <stdbool.h>
 
 typedef struct {
-    // Magnitude cap (Q16.16 fixed point, 0x0000D999 = 0.85)
-    int32_t magnitude_cap;
-    // Drift strength (Q16.16, 0x00001470 = 0.08)
-    int32_t drift_strength;
-    // Maximum drift (Q16.16, 0x00004000 = 0.25)
-    int32_t drift_max;
-    // How often drift target changes (in frames)
-    uint32_t drift_retarget_frames;
-    // Idle threshold (Q16.16, 0x00001999 = 0.10)
-    int32_t idle_threshold;
-    // Enable/disable
-    bool enabled;
-} HumanizerSettings;
-
-typedef struct {
-    HumanizerSettings settings;
-
-    int32_t drift_lx, drift_ly;
-    int32_t drift_rx, drift_ry;
-    int32_t target_lx, target_ly;
-    int32_t target_rx, target_ry;
-    uint32_t retarget_counter_l;
-    uint32_t retarget_counter_r;
-    bool was_idle_l;
-    bool was_idle_r;
     uint32_t rng_state;
+    
+    // Persistent angle offset memories to enable slow, smooth frame-to-frame drifting
+    float wander_angle_l;
+    float wander_angle_r;
+    
+    // Imperfect centering states
+    int16_t drift_lx;
+    int16_t drift_ly;
+    int16_t drift_rx;
+    int16_t drift_ry;
+    uint32_t last_drift_time;
 } Humanizer;
 
 void humanizer_init(Humanizer* h);
-void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry);
+void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry, uint16_t angle_spread, uint16_t deflection_scale, uint16_t deadzone);
 
 #endif
